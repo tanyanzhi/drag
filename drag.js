126d3f92ab8c6144f052ac5ce570278952dcef27
tanyanzhi <345723624@qq.com>
2016-04-06 20:27:21 +0800

    change js new1

--- drag.js
+++ drag.js
@@ -7,5 +7,11 @@ function drag(id){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
 		
+		document.onmousemove = function(ev){
+			
+			obj.style.left = ev.clientX - disX + "px";
+			obj.style.top = ev.clientY - disY + "px";
+			
+		}
 	}
 }
